5d08e39f320c27198d429b786cc7e03527d8f8ef
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-09-20 05:58:06 -0700

    initial commit

---
+++ src/main/java/com/prs/spring/PrsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.prs.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prs/spring/PrsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prs.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
